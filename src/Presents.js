766eb0c7fdcbffa6c756cbe681b1b7271e39d79b
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-11-15 00:10:25 +0100

    Add description for the presents positions

--- src/Presents.js
+++ src/Presents.js
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import './App.css';
+import { data } from "./data";
 
 function Presents(){
+
+    const [present, setPresent] = useState(data);
 
     return(
         <div>
             <h1> Make your choice!</h1>
             <p className="present_title">Here, you can chose, which present you like the most.</p>
+
+            <div>
+                {present.map((present => {
+                    const {id,presentsImg,presentsTitle,presentsDescription,price} = present;
+                }))}
+                <img src={present.length} />
+            </div>
         </div>
     );
 }
