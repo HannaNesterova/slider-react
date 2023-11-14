e4ef7b0acf92ac7074a81d3c52bf9fee01985b78
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-11-14 18:43:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { data } from './data';
+import './App.css';
+
+function App() {
+
+
+
+  return (
+    <div>
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,42 @@
+import christmasPresent from './img/christmasPresent.jpeg';
+import gift from './img/gift.jpeg';
+import presentBox from './img/presentBox.jpeg';
+import person from './img/person.jpeg';
+
+
+export const data = [
+
+    {
+      id: 1,  
+      name: "Shop Simply, Gift Extraordinarily", 
+      description: "Welcome to our online shop, where thoughtful gifting meets convenience! ",
+      titles: "Thoughtful Presents for Every Celebration",
+      image: christmasPresent
+    },
+  
+    {
+      id: 2,
+      name: "Gifts Made Simple, Moments Made Special",
+      description: "Whether you're celebrating a special occasion or simply want to brighten someone's day, we have the perfect gifts to suit every taste and style.",
+      titles: 'Joyful Gifts for Any Day',
+      image: gift
+    },
+  
+    {
+      id: 3,
+      name: "Find. Click. Gift",
+      description: "With hassle-free ordering and secure delivery, we're here to make your gift-giving experience seamless.",
+      titles: 'Presents for Christmas',
+      image: presentBox
+    },
+  
+    {
+      id: 4,
+      name: "Unwrap Happiness, One Click Away.",
+      description: "At our shop, we believe that every gift tells a story. Let your gifts create unforgettable moments for those you care about.",
+      titles: 'Treats for Special Occasions',
+      image: person
+    }
+  ]
+  
+  
