d3455499dffa494548eaf8f671fb979357734f9e
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-11-15 13:51:21 +0100

    Make flex-positioning for Presents component

--- src/Presents.js
+++ src/Presents.js
@@ -1,21 +1,32 @@
 import { useState } from "react";
 import './App.css';
-import { data } from "./data";
+import { dataPresents } from "./dataPresents";
 
 function Presents(){
 
-    const [present, setPresent] = useState(data);
+    const [present, setPresent] = useState(dataPresents);
 
     return(
         <div>
             <h1> Make your choice!</h1>
-            <p className="present_title">Here, you can chose, which present you like the most.</p>
+            <p className="present_title">Here, you can chose, which present you like the most, from all of {present.length} items.</p>
 
-            <div>
+            <div className="flex_presents">
                 {present.map((present => {
-                    const {id,presentsImg,presentsTitle,presentsDescription,price} = present;
+                    const {id, presentsImg,presentsTitle, presentsDescription, price } = present;
+                    return(
+                        <div key={id} className="presentsContainer">
+                            <h4>{id} - {presentsTitle}</h4>
+                            <img src={presentsImg} alt="img" width='300px' height='450px' />
+                            <p>{presentsDescription}</p>
+                            <p>{price}</p>
+                            <button>delete</button>
+                        </div>
+                    )
                 }))}
-                <img src={present.length} />
+            </div>
+            <div className="btn_del_all">
+                <button >DELETE ALL ITEMS</button>
             </div>
         </div>
     );
