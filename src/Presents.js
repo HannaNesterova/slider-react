5e12d990c0eb5c6001270d5a79384330334adc75
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-11-15 14:19:01 +0100

    Use filter for deliting pushed item

--- src/Presents.js
+++ src/Presents.js
@@ -6,27 +6,36 @@ function Presents(){
 
     const [present, setPresent] = useState(dataPresents);
 
+    const removePresent = (id) => {
+        const newPresent = present.filter(item =>
+            item.id !== id);
+            setPresent(newPresent);
+        
+    }
+
     return(
         <div>
             <h1> Make your choice!</h1>
             <p className="present_title">Here, you can chose, which present you like the most, from all of {present.length} items.</p>
 
             <div className="flex_presents">
+
                 {present.map((present => {
                     const {id, presentsImg,presentsTitle, presentsDescription, price } = present;
+
                     return(
                         <div key={id} className="presentsContainer">
                             <h4>{id} - {presentsTitle}</h4>
                             <img src={presentsImg} alt="img" width='300px' height='450px' />
                             <p>{presentsDescription}</p>
                             <p>{price}</p>
-                            <button>delete</button>
+                            <button onClick={() => removePresent(id)}>delete</button>
                         </div>
                     )
                 }))}
             </div>
             <div className="btn_del_all">
-                <button >DELETE ALL ITEMS</button>
+                <button onClick={() => setPresent([])}>DELETE ALL ITEMS</button>
             </div>
         </div>
     );
